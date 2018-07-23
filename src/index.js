be210909905db7fb1824227777c368733faaf9fb
Walter Dal Mut <walter.dalmut@gmail.com>
2018-07-23 21:56:14 +0200

    more effective functional approach

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-const {T, F, __, map, pipe, join, split, equals, ifElse, always, modulo, identity} = require('ramda');
-
-const mod3 = modulo(__, 3)
-const mod5 = modulo(__, 5)
-const mod15 = modulo(__, 15)
-
-const is_mod3 = ifElse(pipe(mod3, equals(0)))(T)(F);
-const is_mod5 = ifElse(pipe(mod5, equals(0)))(T)(F);
-const is_mod15 = ifElse(pipe(mod15, equals(0)))(T)(F);
-
-const to_fizz = ifElse(is_mod3)(always("fizz"))(identity);
-const to_buzz = ifElse(is_mod5)(always("buzz"))(identity);
-const to_fizzbuzz = ifElse(is_mod15)(always("fizzbuzz"))(identity);
-
-const fizzbuzz = pipe(to_fizzbuzz, to_fizz, to_buzz);
+const {T, __, map, pipe, cond, join, split, equals, always, modulo, identity} = require('ramda');
 
 module.exports = pipe(
   split(','),
-  map(fizzbuzz),
+  map(
+    cond([
+      [pipe(modulo(__, 15), equals(0)), always("fizzbuzz")],
+      [pipe(modulo(__, 3), equals(0)), always("fizz")],
+      [pipe(modulo(__, 5), equals(0)), always("buzz")],
+      [T, identity],
+    ])
+  ),
   join(',')
 );
 
